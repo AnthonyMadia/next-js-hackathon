b837b3d05f7088f3566e76325ca4293b02cdeb1a
Anthony Madia <aamadia24@gmail.com>
2022-04-29 16:23:58 -0400

    Complete

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useWeb3Contract } from "react-moralis"
+import { abi } from "../constants/abi"
 
 export default function Home() {
+
+  const { runContractFunction } = useWeb3Contract(
+    {
+      abi: abi,
+      contractAdd: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      functionName: "store",
+      params: {
+        _favoriteNumber: 778,
+      },
+    }
+  )
+  
   return (
-    <>
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <h1>HELLO FROM INDEX</h1>
+      <button onClick={() => runContractFunction()}>Store 778</button>
     </div>
-    <h1>Next js</h1>
-    </>
   )
 }
